0a12829055563f8028a0c998f3f1e2829093e052
JustAD3v <gloryjust@outlook.fr>
2026-03-27 07:59:57 +0100

    feat: Profil Banner added

--- my-blog/src/app/app.component.ts
+++ my-blog/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { BannerComponent } from './components/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-blog';
 }
---
+++ my-blog/src/app/components/banner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-banner',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: "./banner.component.html",
+  styleUrls: ['./banner.component.css']
+})
+
+export class BannerComponent {
+}
